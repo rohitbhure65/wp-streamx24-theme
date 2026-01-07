fd7c6166824267e6604585fc930717fa3bb06f4b
Rohit Bhure <rohitbhure.cse@gmail.com>
2026-01-07 21:13:13 +0530

    created custom page

--- page.php
+++ page.php
@@ -3,9 +3,13 @@
     the_post();
 ?>
 
-<?php 
+<div class="sx-container">
+
+    <?php 
+    the_title('<h1 class="sx-page-title">', '</h1>');
  echo the_content();
-?>
+ ?>
+ </div>
 
 <?php 
     get_footer();
